445d131ea397ce5e7da6a67097ca0830b708955d
Diogo Lima <diogoslva013@gmail.com>
2022-09-30 14:39:07 -0300

    creating server with express

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
-const http= require('http');
+const express= require('express');
 
-let server = http.createServer((req, res) => {
+let app = express();
 
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
+app.get('/', (req, res) => {
 
-    switch (req.url){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá</h1>')
 
-        case '/':
+});
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Olá</h1>')
+app.get('/users',(req, res) => {
 
-        break;
-
-        case '/users':
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
             users:[{
                 name:"Hcode",
                 email:'contato@hcode.com.br',
                 id:1
             }]
-        }));
-
-        break;
-
-    }
+        });
 
 });
 
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
 
     console.log("servidor rodando!");
 
