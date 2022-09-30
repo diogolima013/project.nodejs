66b28233c85ad991ac5f835efab65a0067f0cea1
Diogo Lima <diogoslva013@gmail.com>
2022-09-30 15:29:25 -0300

    Listing NeDB database users and orimizing the code

--- routes/users.js
+++ routes/users.js
@@ -8,29 +8,31 @@ let db = new NeDB({
 
 module.exports = (app)=>{
     
-app.get('/users',(req, res) => {
+let route = app.route('/users');
+    
+route.get((req, res) => {
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-            users:[{
-                name:"Hcode",
-                email:'contato@hcode.com.br',
-                id:1
-            }]
-        });
+    db.find({}).sort({name:1}).exec((err, users)=>{
 
+        if (err){
+           app.utils.error.send(err, req, res)
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+                    users
+                });
+        
+            }
+    })
 });
 
-app.post('/users', (req, res)=>{
+route.post((req, res)=>{
 
     db.insert(req.body, (err, user)=>{
 
         if(err){
-            console.log(`error:${err}`);
-            res.status(400).json({
-                error: err
-            })
+            app.utils.error.send(err, req, res)
         } else {
 
             res.status(200).json(user);
